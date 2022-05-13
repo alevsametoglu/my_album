a27106402c6d44847b5d4c169ecbad492b1f9478
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-13 18:51:05 +0200

    export components

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,17 @@
 import Header from "./Header/Header";
+import SearchInput from "./SearchInput/SearchInput";
+import LetterAnimation from "./LetterAnimation/LetterAnimation";
+import Artist from "./Artist/Artist";
+import Track from "./Track/Track";
+import Player from "./Player/Player";
+import UserArtistList from "./UserArtistList/UserArtistList";
 
-export { Header };
+export {
+  Header,
+  SearchInput,
+  LetterAnimation,
+  Artist,
+  Track,
+  Player,
+  UserArtistList,
+};
