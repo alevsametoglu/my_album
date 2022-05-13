9550cc7e3e6db3c62bd3782a6d13f6ed975e4732
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-13 18:49:38 +0200

    add routing to page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { MainLayout } from "./Layout/MainLayout";
+import { HomePage } from "./Pages/HomePage";
+
+import { Callback } from "./Pages/Callback";
+import { Dashboard } from "./Pages/Dashboard";
+import { AlbumsPage } from "./Pages/AlbumsPage";
+import { TrackPage } from "./Pages/TrackPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/callback" element={<Callback />} />
+          <Route path="/artist/:id" element={<AlbumsPage />} />
+          <Route path="/artist/:id/track" element={<TrackPage />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
