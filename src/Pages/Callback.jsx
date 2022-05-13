552d140b7e939086953f6fd7a8098f7019759b59
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-13 23:39:23 +0200

    create callback page

---
+++ src/Pages/Callback.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router";
+import api from "../Api";
+import { useNavigate } from "react-router-dom";
+
+import { authActions } from "../redux/authSlice";
+import "./Callback.scss";
+import { LetterAnimation } from "../Components";
+
+export const Callback = () => {
+  const dispatch = useDispatch();
+  const authState = useSelector((store) => store.auth);
+  const params = useLocation();
+  const navigate = useNavigate();
+  const urlParams = new Map();
+  const paramArray = params.hash.split("&");
+
+  paramArray?.forEach((param) =>
+    urlParams.set(param.split("=")[0], param.split("=")[1])
+  );
+
+  useEffect(() => {
+    api.setDefaultToken(urlParams.get("#access_token"));
+    dispatch(authActions.setToken({ token: urlParams.get("#access_token") }));
+    api.getCurrentUser();
+    dispatch(authActions.loadProfile());
+
+    setTimeout(() => {
+      navigate("/dashboard");
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="callback">
+      <h1>Welcome {authState.displayName}!</h1>
+      <LetterAnimation />
+    </div>
+  );
+};
