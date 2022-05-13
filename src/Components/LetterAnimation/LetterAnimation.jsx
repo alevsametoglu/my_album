93aef7e0afe7112af80ea1ab753b6431815a67d1
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-13 23:36:04 +0200

    create animation

---
+++ src/Components/LetterAnimation/LetterAnimation.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./LetterAnimation.scss";
+
+const LetterAnimation = () => {
+  return (
+    <div className="letter-image">
+      <div className="animated-mail">
+        <div className="back-fold"></div>
+        <div className="letter">
+          <div className="letter-border"></div>
+          <div className="letter-title"></div>
+          <div className="letter-context"></div>
+          <div className="letter-stamp">
+            <div className="letter-stamp-inner"></div>
+          </div>
+        </div>
+        <div className="top-fold"></div>
+        <div className="body"></div>
+        <div className="left-fold"></div>
+      </div>
+      <div className="shadow"></div>
+    </div>
+  );
+};
+export default LetterAnimation;
