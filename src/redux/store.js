558b18b7ed203451f4d976bf9508c8c2f06a64a1
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-13 23:35:15 +0200

    add reducer to store

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { authReducer } from "./authSlice.js";
+
+import { userReducer } from "./userSlice.js";
+
+export default configureStore({
+  reducer: {
+    user: userReducer,
+    auth: authReducer,
+  },
+});
