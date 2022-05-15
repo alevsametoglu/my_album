10359ee7021afa5788bb7dc0454f3288963f8146
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-15 11:07:06 +0200

    create util functions for api

---
+++ src/app/util.js
@@ -0,0 +1,41 @@
+const getArtistsFromItems = (items) => {
+  return items.map((item) => ({ name: item.name, image: item?.images?.[0]?.url, id: item.id }));
+};
+const getTracksFromItems = (items) => {
+  console.log(items);
+  return items.map((item) => ({
+    id: item.id,
+    name: item.name,
+    duration: msToMinuteString(item.duration_ms),
+    artistName: item?.artists.map((x) => x.name).join(", ") || "",
+    albumImage: item?.album?.images?.[0]?.url || "",
+    albumName: item?.album?.name || "",
+    uri: item.uri,
+  }));
+};
+
+const getAlbumsFromItems = (items) => {
+  console.log("albums", items);
+  return items.map((item) => ({
+    id: item.id,
+    name: item.name,
+    image: item.images?.[0]?.url || "",
+    date: item.release_date,
+  }));
+};
+
+export const util = {
+  getAlbumsFromItems,
+  getArtistsFromItems,
+  getTracksFromItems,
+};
+
+const msToMinuteString = (valueMs) => {
+  const valueSecond = Math.round((Math.round(valueMs / 1000) / 60) * 100) / 100;
+  const timeArray = valueSecond.toString().split(".");
+  const minuteString = timeArray[0].padStart(2, "0");
+  const secondString = Math.floor(Number(timeArray[1]) * 0.6)
+    .toString()
+    .padStart(2, "0");
+  return `${minuteString}:${secondString}`;
+};
