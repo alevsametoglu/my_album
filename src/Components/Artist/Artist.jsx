ddff5dae008c214fed0e3af5be526112d0bea2ea
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-15 11:17:18 +0200

    update artist component

--- src/Components/Artist/Artist.jsx
+++ src/Components/Artist/Artist.jsx
@@ -1,21 +1,14 @@
-import React from "react";
 import "./Artist.scss";
+import React from "react";
 import { Link } from "react-router-dom";
 
-const Artist = ({ name, image, id, navigate }) => {
+const Artist = ({ image, name, id }) => {
   return (
-    <Link to={navigate} className="album">
-      <div className="image">
-        <img srcSet={image} alt="" />
-      </div>
-      <div>
-        <div className="infos">
-          <div className="title">
-            <span>{name}</span>
-          </div>
-        </div>
-      </div>
+    <Link to={`artist/${id}`} className="artist">
+      <img className="profile-image" src={image} alt={name} />
+      <span className="name">{name}</span>
     </Link>
   );
 };
+
 export default Artist;
