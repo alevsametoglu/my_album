31159b8b7b1d4484ce4b78e96687555532de48e2
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-15 13:58:34 +0200

    add search states

--- src/redux/appSlice.js
+++ src/redux/appSlice.js
@@ -4,11 +4,22 @@ export const appSlice = createSlice({
   name: "app",
   initialState: {
     selectedTrackUri: "",
+    searchResult: [],
+    searchKey: "",
   },
   reducers: {
     selectTrack: (state, action) => {
       console.log(action);
       state.selectedTrackUri = action.payload;
+    },
+    setSearchKey: (state, action) => {
+      state.searchKey = action.payload;
+    },
+    setSearchResult: (state, action) => {
+      state.searchResult = action.payload;
+    },
+    clearSearchKey: (state) => {
+      state.searchKey = "";
     },
   },
 });
