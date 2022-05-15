0eaa3dc690a4c104a7aae1e229355af03e76c7b2
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-15 11:17:35 +0200

    create profile

---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { SearchInput } from "..";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { authActions } from "../../redux/authSlice";
+import { userActions } from "../../redux/userSlice";
+import "./Profile.scss";
+
+const Profile = () => {
+  const authState = useSelector((s) => s.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const logout = () => {
+    dispatch(authActions.logout());
+    dispatch(userActions.clearUserInfo());
+    navigate("/");
+  };
+  return (
+    <div className="profile">
+      <div className="profile-info">
+        <img className="profile-image" src={authState.image} alt={authState.displayName} />
+        <div className="profile-name">
+          <h1 style={{ color: "white" }}>{authState.displayName}</h1>
+          {authState.isAuth && (
+            <span className="logout" onClick={logout}>
+              logout
+            </span>
+          )}
+        </div>
+      </div>
+      <div className="search">
+        <SearchInput />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
