33b95a376abe0c662d5ca2fc27ec1329bebc96ce
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-15 11:20:03 +0200

    prettier style

--- src/Components/UserArtistList/UserArtistList.jsx
+++ src/Components/UserArtistList/UserArtistList.jsx
@@ -1,24 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Artist } from "..";
-import api from "../../Api";
+import api from "../../app/api";
 import "./UserArtistList.scss";
 
 const UserArtistList = () => {
   const [myArtists, setMyArtists] = useState([]);
   useEffect(() => {
-    api.getUserArtist().then((response) => setMyArtists(response));
+    api.user.getFollowedArtist().then((response) => setMyArtists(response));
   }, []);
-  console.log(myArtists);
 
   return (
     <div>
       {myArtists.map((artist) => (
-        <Artist
-          key={artist.id}
-          name={artist.name}
-          image={artist.images?.[0]?.url}
-          navigate={`/artist/${artist.id}`}
-        />
+        <Artist key={artist.id} name={artist.name} image={artist.images?.[0]?.url} navigate={`/artist/${artist.id}`} />
       ))}
     </div>
   );
