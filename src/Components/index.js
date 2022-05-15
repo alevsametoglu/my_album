c0412f4635920c1a87f4ea8abb7ab19425c1b869
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-15 11:12:17 +0200

    export components

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,9 +1,26 @@
-import Header from "./Header/Header";
 import SearchInput from "./SearchInput/SearchInput";
 import LetterAnimation from "./LetterAnimation/LetterAnimation";
-import Artist from "./Artist/Artist";
-import Track from "./Track/Track";
 
 import UserArtistList from "./UserArtistList/UserArtistList";
+import FavoriteArtistList from "./FavoriteArtistList";
+import TrackList from "./TrackList";
+import Track from "./Track/Track";
+import Artist from "./Artist/Artist";
+import SpotifyPlayer from "./SpotifyPlayer";
+import Profile from "./Profile/Profile";
+import AlbumList from "./AlbumList/AlbumList";
+import SectionHeader from "./SectionHeader";
 
-export { Header, SearchInput, LetterAnimation, Artist, Track, UserArtistList };
+export {
+  Profile,
+  SearchInput,
+  LetterAnimation,
+  Artist,
+  UserArtistList,
+  Track,
+  FavoriteArtistList,
+  TrackList,
+  SpotifyPlayer,
+  AlbumList,
+  SectionHeader,
+};
