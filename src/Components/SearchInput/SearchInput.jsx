f67fbbd4b4c6bcfebe596fb55b05463b085398bc
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-15 19:02:59 +0200

    clear input value

--- src/Components/SearchInput/SearchInput.jsx
+++ src/Components/SearchInput/SearchInput.jsx
@@ -1,16 +1,24 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import api from "../../app/api";
 import { appActions } from "../../redux/appSlice";
 import "./SearchInput.scss";
 
 const SearchInput = () => {
   const dispatch = useDispatch();
+  const appState = useSelector((s) => s.app);
+  const inputRef = useRef();
 
   const handleSearch = (value) => {
     dispatch(appActions.setSearchKey(value));
     api.searchArtists(value).then((result) => dispatch(appActions.setSearchResult(result)));
   };
+
+  useEffect(() => {
+    if (appState.searchKey === "") {
+      inputRef.current.value = "";
+    }
+  }, [appState.searchKey]);
 
   return (
     <div className="search-box">
@@ -18,6 +26,7 @@ const SearchInput = () => {
         <i className="fas fa-search"></i>
       </button>
       <input
+        ref={inputRef}
         type="text"
         className="input-search"
         placeholder="Search an artist..."
