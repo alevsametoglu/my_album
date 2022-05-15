fd690b0ef1a24aaac0f09e30c468c8baad10e077
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-15 11:09:41 +0200

    add tarck and artist state

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,42 +1,69 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import api from "../Api";
-const updateProfile = createAsyncThunk(
-  "user/updateProfile",
-  async ({ firstName, lastName }) => {
-    const response = await api.updateProfile(firstName, lastName);
-    console.log(response);
-    return response;
-  }
-);
+import api from "../app/api";
 
 export const userSlice = createSlice({
   name: "user",
   initialState: {
-    firstName: "",
-    lastName: "",
+    favTrack: {
+      errorMessage: "",
+      loading: false,
+      items: [],
+    },
+    favArtist: {
+      errorMessage: "",
+      loading: false,
+      items: [],
+    },
   },
   reducers: {
-    setProfile: (state, action) => {
-      state.firstName = action.payload.firstName;
-      state.lastName = action.payload.lastName;
-      console.log(action.payload);
+    clearUserInfo: (state) => {
+      state.favArtist.items = [];
+      state.favTrack.items = [];
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(updateProfile.fulfilled, (state, action) => {
-      console.log(action);
-      state.firstName = action.payload.firstName;
-      state.lastName = action.payload.lastName;
+    builder.addCase(loadFavoriteTracks.pending, (state) => {
+      state.favTrack.loading = true;
+      state.favTrack.errorMessage = "";
+      state.favTrack.items = [];
     });
-    builder.addCase(updateProfile.rejected, (state, action) => {
-      console.log(action.payload);
-      console.log("aaaaaaa");
+    builder.addCase(loadFavoriteTracks.fulfilled, (state, action) => {
+      state.favTrack.loading = false;
+      state.favTrack.items = action.payload;
+    });
+    builder.addCase(loadFavoriteTracks.rejected, (state, action) => {
+      state.favTrack.loading = false;
+      state.favTrack.errorMessage = action.message;
+    });
+
+    builder.addCase(loadFavoriteArtists.pending, (state) => {
+      state.favArtist.loading = true;
+      state.favArtist.errorMessage = "";
+      state.favArtist.items = [];
+    });
+    builder.addCase(loadFavoriteArtists.fulfilled, (state, action) => {
+      state.favArtist.loading = false;
+      state.favArtist.items = action.payload;
+    });
+    builder.addCase(loadFavoriteArtists.rejected, (state, action) => {
+      state.favArtist.loading = false;
+      state.favArtist.errorMessage = action.message;
     });
   },
 });
 
+const loadFavoriteTracks = createAsyncThunk("user/loadFavoriteTracks", async () => {
+  const response = await api.user.getTopTracks();
+  return response;
+});
+
+const loadFavoriteArtists = createAsyncThunk("user/loadFavoriteArtists", async () => {
+  const response = await api.user.getTopArtists();
+  return response;
+});
+
 // Action creators are generated for each case reducer function
-const userActions = { ...userSlice.actions, updateProfile };
+const userActions = { ...userSlice.actions, loadFavoriteArtists, loadFavoriteTracks };
 const userReducer = userSlice.reducer;
 
 export { userActions, userReducer };
