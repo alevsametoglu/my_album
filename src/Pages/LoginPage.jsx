e89d59acc63c768abe839570186f92649922593d
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-15 16:20:17 +0200

    change callback uri

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import logo from "./logoSpotify.png";
 import "./LoginPage.scss";
 
 const client_id = "558c8cdf1eda4ca88e35b68a25c576e2";
-var redirect_uri = "http://localhost:3000/callback";
+var redirect_uri = "https://thriving-cheesecake-af8741.netlify.app/callback";
 var scope =
   "ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read";
 
