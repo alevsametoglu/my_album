1e28ab9ac8aa06f0a430269a1a1726927a619379
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-15 11:08:44 +0200

    add app reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { appReducer } from "./appSlice.js";
 import { authReducer } from "./authSlice.js";
 
 import { userReducer } from "./userSlice.js";
@@ -7,5 +8,6 @@ export default configureStore({
   reducer: {
     user: userReducer,
     auth: authReducer,
+    app: appReducer,
   },
 });
